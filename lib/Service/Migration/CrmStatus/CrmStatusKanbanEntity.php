98bda67ddc8703eb1ea1f425ba2447fb7a4ff81d
a.odegov <a.odegov@oos.ru>
2025-06-24 17:01:29 +0300

    release v1.0.2

--- lib/Service/Migration/CrmStatus/CrmStatusKanbanEntity.php
+++ lib/Service/Migration/CrmStatus/CrmStatusKanbanEntity.php
@@ -5,6 +5,6 @@ namespace Base\Module\Service\Migration\CrmStatus;
 interface CrmStatusKanbanEntity
 {
     public static function getCategoryId(): int;
-    public static function getColor(): string;
-    public static function getSemantics(): string;
+    public static function getColor(): ?string;
+    public static function getSemantics(): ?string;
 }
